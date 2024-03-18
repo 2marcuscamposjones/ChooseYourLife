6503d090ae611898f2c088cfbfe9783ddbe1df0e
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-03-18 12:08:23 -0500

    Intital Printstatements&Variable - none Ai

--- ChooseYourLife1.0.py
+++ ChooseYourLife1.0.py
@@ -1,3 +1,12 @@
 # Programmer: Marcus Campos-Jones
 # Date: 3.15.2024
 # Program: Choose your Life
+print("Our mission is to get out of the woods!.")
+print("Make a wrong turn or choice and you could be in a fight for your life")
+
+health = 25
+dcrsHlthMjr = 5
+dcrsHlthMnr = 2
+incrsHkthMJr = 5
+incrsHkthMnr = 2
+
