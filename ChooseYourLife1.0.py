ad905db68afe329c12cca8beb6d48d20f5e9a962
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-03-18 12:10:43 -0500

    Used Ai to comment

--- ChooseYourLife1.0.py
+++ ChooseYourLife1.0.py
@@ -1,12 +1,14 @@
 # Programmer: Marcus Campos-Jones
 # Date: 3.15.2024
 # Program: Choose your Life
+
+# Print initial mission information
 print("Our mission is to get out of the woods!.")
 print("Make a wrong turn or choice and you could be in a fight for your life")
 
+# Initialize player's health and health modifiers
 health = 25
-dcrsHlthMjr = 5
-dcrsHlthMnr = 2
-incrsHkthMJr = 5
-incrsHkthMnr = 2
-
+dcrsHlthMjr = 5  # Major health decrease
+dcrsHlthMnr = 2  # Minor health decrease
+incrsHkthMJr = 5  # Major health increase
+incrsHkthMnr = 2  # Minor health increase
